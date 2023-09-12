925cc4e1094a5e6afd5a33478b3717184c378146
Jesse Nguyen <67484662+nguyenjessev@users.noreply.github.com>
2023-09-11 22:17:47 -0400

    Update build

--- build/_app/immutable/chunks/paths.f51081ba.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_jbzasb)==null?void 0:s.base)??"/frontend-mentor-projects";var e;const a=((e=globalThis.__sveltekit_jbzasb)==null?void 0:e.assets)??t;export{a,t as b};
