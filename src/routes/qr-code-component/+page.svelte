e2a4befc940f379311130ce99272d5495df8f8d5
Jesse Nguyen <67484662+nguyenjessev@users.noreply.github.com>
2023-09-11 21:17:18 -0400

    Place modal in center

--- src/routes/qr-code-component/+page.svelte
+++ src/routes/qr-code-component/+page.svelte
@@ -1,9 +1,20 @@
-<div class="container" />
+<div class="container">
+  <main class="modal" />
+</div>
 
 <style>
   .container {
     background: hsl(212, 45%, 89%);
+    display: grid;
     height: 100dvh;
+    place-items: center;
     width: 100dvw;
+  }
+
+  .modal {
+    background: white;
+    height: 100px;
+    max-width: 320px;
+    width: 100%;
   }
 </style>
