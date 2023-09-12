0d10a58410a036d0684da9f20f1ca17b307b28cb
Jesse Nguyen <67484662+nguyenjessev@users.noreply.github.com>
2023-09-11 21:25:09 -0400

    Round corners

--- src/routes/qr-code-component/+page.svelte
+++ src/routes/qr-code-component/+page.svelte
@@ -1,6 +1,10 @@
 <div class="container">
   <main class="modal">
-    <img src="/qr-code-component/image-qr-code.png" alt="QR code" />
+    <img
+      src="/qr-code-component/image-qr-code.png"
+      alt="QR code"
+      class="qr-code"
+    />
   </main>
 </div>
 
@@ -15,9 +19,15 @@
 
   .modal {
     background: white;
+    border-radius: 20px;
     display: flex;
     flex-direction: column;
     max-width: 320px;
+    padding: 16px;
     width: 100%;
+  }
+
+  .qr-code {
+    border-radius: 10px;
   }
 </style>
