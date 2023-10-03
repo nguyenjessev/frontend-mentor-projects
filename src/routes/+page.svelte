4e6158bfbca2b412488da9fc5e7d31da391454c7
Jesse Nguyen <67484662+nguyenjessev@users.noreply.github.com>
2023-10-02 20:52:25 -0400

    Add results summary component link to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,7 @@
 <h1>Projects</h1>
 <ul>
   <li><a href="{base}/qr-code-component">QR Code Component</a></li>
+  <li>
+    <a href="{base}/results-summary-component">Results Summary Component</a>
+  </li>
 </ul>
