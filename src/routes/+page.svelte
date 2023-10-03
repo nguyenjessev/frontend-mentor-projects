cef7a61bde2dfd1ae0039cda3a52f703bade67d3
Jesse Nguyen <67484662+nguyenjessev@users.noreply.github.com>
2023-10-02 20:55:56 -0400

    Add title to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
   import { base } from '$app/paths';
 </script>
 
+<svelte:head>
+  <title>Frontend Mentor Projects</title>
+</svelte:head>
+
 <h1>Projects</h1>
 <ul>
   <li><a href="{base}/qr-code-component">QR Code Component</a></li>
